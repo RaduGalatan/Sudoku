9399f5e6d67ea5ed98a5683e522e36f098773593
RaduG <radug@ensemble.com>
2022-04-05 13:02:44 +0300

    Deleted win and lose activities.Replaced with a single outcome activity.

--- app/src/main/java/com/example/sudoku/activity/GameResultActivity.java
+++ app/src/main/java/com/example/sudoku/activity/GameResultActivity.java
@@ -1,27 +1,35 @@
 package com.example.sudoku.activity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.sudoku.R;
+import com.example.sudoku.outcome.GameOutcome;
+import com.example.sudoku.outcome.Outcome;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.sudoku.DifficultyLevel;
-import com.example.sudoku.R;
-
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
-public class VictoryActivity extends AppCompatActivity {
+public class GameResultActivity extends AppCompatActivity {
+
+    private GameOutcome outcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_victory);
+        setContentView(R.layout.activity_game_result);
 
-        long limit=getIntent().getLongExtra("timeLimit",0);
+        outcome=getIntent().getParcelableExtra("outcome");
+
+        long limit=outcome.getTimeLimit();
+
         TextView timeView = findViewById(R.id.time);
+        TextView outcomeView=findViewById(R.id.outcomeText);
+
+        outcomeView.setText(outcome.getOutcome().toString());
 
         if(limit!=0) {
             NumberFormat f = new DecimalFormat("00");
@@ -31,7 +39,7 @@ public class VictoryActivity extends AppCompatActivity {
             //time limit in mm:ss format
             String arg1 = f.format(limitMin) + ":" + f.format(limitSec);
 
-            long elapsedTime = getIntent().getLongExtra("time", 0);
+            long elapsedTime = outcome.getTimeElapsed();
             long elapsedMin = (elapsedTime / 60000) % 60;
             long elapsedSec = (elapsedTime / 1000) % 60;
             //elapsed time in mm:ss format
@@ -46,12 +54,12 @@ public class VictoryActivity extends AppCompatActivity {
             timeView.setText("");
         }
 
-        Button btn=(Button) findViewById(R.id.restartButton);
+        Button btn=(Button) findViewById(R.id.restartButton3);
 
         btn.setOnClickListener(view -> {
-            Intent i = new Intent(VictoryActivity.this, MainMenuActivity.class);
+            Intent i = new Intent(GameResultActivity.this, MainMenuActivity.class);
             startActivity(i);
-            VictoryActivity.this.finish();
+            GameResultActivity.this.finish();
         });
     }
 }
